6347e69cc18e0e930a3bc6fc905f44291b38a91c
Dennis Israelsson <dennis.israelsson@atea.se>
2020-12-30 20:41:00 +0100

    Day 7: Handy Haversacks

--- day_07.py
+++ day_07.py
@@ -4,36 +4,84 @@ from re import compile as re_compile
 from aocd import get_data
 
 
+class Bag():
+    """ Bag object for a given colour with outer and inner references """
+    def __init__(self, colour):
+        self.colour = colour
+        self.outer = list()
+        self.inner = list()
+
+    def add_outer(self, outer):
+        """ Add outer bag to self """
+        self.outer.append(outer)
+
+    def add_inner(self, inner, amount):
+        """ Add inner bag to self """
+        self.inner.append([inner, amount])
+
+    def count_outer_bag(self):
+        """ Return count of all unique outer bags """
+        possible_bags = self.find_outer_bag()
+        return len(dict.fromkeys(possible_bags))
+
+    def find_outer_bag(self):
+        """ Find all possible outer bags """
+        count = list()
+        for bag in self.outer:
+            count.append(bag)
+            recursive = bag.find_outer_bag()
+            count += recursive
+
+        return count
+
+    def total_bags(self):
+        """ Get total amount of bags needed """
+        count = 0
+        for bag, amount in self.inner:
+            recursive = bag.total_bags()
+            count += amount + amount * recursive
+
+        return count
+
+
 def parse_input(puzzle_data):
     """ Return puzzle in structured data form """
     structured_puzzle = dict()
-    # pattern = re_compile(r"^(\w+\s\w+)\sbags\scontain\s(?:(\d)\s(\w+\s\w+)\sbags?(?:,\s)?)+\.$")
     for line in puzzle_data.split("\n"):
-        parent, children = line.split(" bags contain ")
-        structured_puzzle[parent] = dict()
-        structured_puzzle[parent]["children"] = list()
-        # print(parent)
-        for child in children.split(", "):
-            if "no other bags." in child:
-                structured_puzzle[parent]["children"] = None
+        outer_line, inner_line = line.split(" bags contain ")
+
+        if outer_line not in structured_puzzle:
+            outer_bag = Bag(outer_line)
+            structured_puzzle[outer_line] = outer_bag
+        else:
+            outer_bag = structured_puzzle[outer_line]
+
+        for inner_part in inner_line.split(", "):
+            if "no other bags." in inner_part:
+                continue
+
+            pattern = re_compile(r"^(?P<amount>\d)+\s(?P<colour>\w+\s\w+).*$")
+            matches = pattern.search(inner_part)
+            amount = matches.group("amount")
+            colour = matches.group("colour")
+
+            if colour not in structured_puzzle:
+                inner_bag = Bag(colour)
+                structured_puzzle[colour] = inner_bag
             else:
-                structured_puzzle[parent]["children"].append(child)
+                inner_bag = structured_puzzle[colour]
+
+            inner_bag.add_outer(outer_bag)
+            outer_bag.add_inner(inner_bag, int(amount))
+
     return structured_puzzle
 
 
 def main():
     """ Solve the puzzle """
-    # puzzle_input = parse_input(get_data(day=7))
-    puzzle_input = ("light red bags contain 1 bright white bag, 2 muted yellow bags.\n"
-                    "dark orange bags contain 3 bright white bags, 4 muted yellow bags.\n"
-                    "bright white bags contain 1 shiny gold bag.\n"
-                    "muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.\n"
-                    "shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.\n"
-                    "dark olive bags contain 3 faded blue bags, 4 dotted black bags.\n"
-                    "vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.\n"
-                    "faded blue bags contain no other bags.\n"
-                    "dotted black bags contain no other bags.")
-    print(parse_input(puzzle_input))
+    puzzle_input = parse_input(get_data(day=7))
+    print(puzzle_input["shiny gold"].count_outer_bag())
+    print(puzzle_input["shiny gold"].total_bags())
 
 
 if __name__ == "__main__":
