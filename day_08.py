66d4fb1ee3da43e57623468abce8a49277012110
denngie <dennis.israelsson@gmail.com>
2021-01-03 21:16:43 +0100

    Day 8: Handheld Halting

--- day_08.py
+++ day_08.py
@@ -12,61 +12,55 @@ def parse_input(puzzle_data):
     return structured_puzzle
 
 
-def execute_instructions(code):
-    """ Return accumulator after code has been executed """
-    accumulator = 0
-    parsed_instructions = list()
+def fix_instruction(code):
+    """ Try to fix the broken code """
     iterator = 0
-    previous_iterator = 0
+    accumulator = 0
     while iterator < len(code):
         argument = code[iterator][0]
-
-        if iterator in parsed_instructions:
-            print("Loop detected, changing previous step")
-            if code[previous_iterator][0] == "jmp":
-                argument = "nop"
-            else:
-                argument = "jmp"
-            print("Current: {}, previous: {}".format(iterator, previous_iterator))
-            break
-            iterator = previous_iterator
-        else:
-            parsed_instructions.append(iterator)
-
-        value = code[iterator][1]
         if argument == "jmp":
-            previous_iterator = iterator
-            iterator = eval("{} {}".format(iterator, value))
+            code[iterator][0] = "nop"
+        elif argument == "nop":
+            code[iterator][0] = "jmp"
+        try:
+            accumulator = process_instructions(code)
+            break
+        except SystemError:
+            # Reset changed argument and continue
+            code[iterator][0] = argument
+            iterator += 1
             continue
-        if argument == "acc":
-            accumulator = eval("{} {}".format(accumulator, value))
-        if argument == "nop":
-            previous_iterator = iterator
-        iterator += 1
     return accumulator
 
 
 def process_instructions(code):
     """ Execute instruction """
+    iterator = 0
     accumulator = 0
     parsed_instructions = list()
-    iterator = 0
     while iterator < len(code):
         argument = code[iterator][0]
         value = code[iterator][1]
 
         if iterator in parsed_instructions:
-            print("Loop detected!")
-            break
-        else:
-            parsed_instructions.append(iterator)
+            # print("Loop detected! Last value was {}".format(accumulator))
+            raise SystemError
+        parsed_instructions.append(iterator)
 
-        if argument == "jmp":
-            iterator = eval("{} {}".format(iterator, value))
-            continue
-        if argument == "acc":
-            accumulator = eval("{} {}".format(accumulator, value))
-        iterator += 1
+        operation = value[0]
+        number = int(value[1:])
+        if operation == "+":
+            if argument == "jmp":
+                iterator += number
+            elif argument == "acc":
+                accumulator += number
+        elif operation == "-":
+            if argument == "jmp":
+                iterator -= number
+            elif argument == "acc":
+                accumulator -= number
+        if argument != "jmp":
+            iterator += 1
     return accumulator
 
 
@@ -81,8 +75,8 @@ def main():
                                "acc +1\n"
                                "jmp -4\n"
                                "acc +6")
-    # puzzle_input = parse_input(get_data(day=8))
-    print(process_instructions(puzzle_input))
+    puzzle_input = parse_input(get_data(day=8))
+    print(fix_instruction(puzzle_input))
 
 
 if __name__ == "__main__":
